db2a63caf48a7fb5815ee0505af6b209096204c7
Preet Rana <preetrana@Preets-MacBook-Air.local>
2025-06-15 17:27:57 +0530

    created user interface

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
 
-    
+    cout<<"======== Bank Management System ========"<<endl;
+    cout<<"1. Create Account\n";
+    cout<<"2. Enter your Account\n";
+    cout<<"3. See your account details\n";
+    cout<<"4. Deposite amount\n";
+    cout<<"5. Withdraw amount\n";
+    cout<<"6. Update your account details\n";
+    cout<<"7. Exit your Account\n";
 
     return 0;
 }
